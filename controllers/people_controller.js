718efe203ded3735e068b23ef6bbc11c2936b30d
wen <wen@wens-MacBook-Pro.local>
2017-03-25 14:10:41 -0500

    solution

---
+++ controllers/people_controller.js
@@ -0,0 +1,33 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+
+router.get('/', function(req, res) {
+  models.Person.findAll({
+    include: [ models.Task ]
+  }).then(function(people) {
+    res.render('people/index', {
+      user_id: req.session.user_id,
+      email: req.session.user_email,
+      logged_in: req.session.logged_in,
+      people: people
+    });
+  });
+});
+
+router.post('/create', function(req, res) {
+  models.Person.create({
+    name: req.body.name
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+router.post('/delete', function(req, res) {
+  models.Person.delete({
+    name: req.body.name
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+module.exports = router;
---
+++ controllers/tasks_controller.js
@@ -0,0 +1,23 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+
+router.post('/:person_id/tasks/create', function (req, res) {
+  models.Task.create({
+    task: req.body.task,
+    person_id: req.params.person_id
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+router.post('/:person_id/tasks/delete', function(req, res){
+	models.Task.delete({
+		task: req.body.task,
+		person_id: req.params.person_id
+	}).then(function(){
+		res.redirect("/");
+	})
+})
+
+module.exports = router;
